6f55f63f93122cd0fc3cf20070adaf746d4f39ff
leiweilo <leiweilo@qq.com>
2022-08-12 16:25:58 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+// 导入全局样式表
+import '@/assets/css/global.css'
+// 导入阿里字体图标
+import '@/assets/fonts/iconfont.css'
+
+// 导入 axios
+import axios  from "axios"
+// 为 axios 设置一下请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 弹框提示组件有些不一样，它需要全局挂载到 Vue 身上, $message 名字可以自己取，官方用message
+// 现在好像官方自动帮我们挂载了$message
+Vue.prototype.$message = Message
